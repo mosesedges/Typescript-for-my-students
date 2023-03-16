ff44035ba408fa759b728bc254c77af3c8cd8597
Moses  Ejele <mosesedges@gmail.com>
2023-03-16 20:49:36 +0000

    timer assignment

---
+++ counter.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var timer = document.getElementById("timer");
+var greeting = "Hello world!";
+var d = new Date();
+timer.innerHTML = greeting;
---
+++ counter.ts
@@ -0,0 +1,5 @@
+export {}
+const timer = document.getElementById("timer")!;
+let greeting : string = "Hello world!";
+const d = new Date();
+timer.innerHTML= greeting; 
