201e19428b5097b015739b6d3a6a124e052818fb
Moses  Ejele <mosesedges@gmail.com>
2023-03-21 22:30:50 +0000

    typescript changes

--- counter.js
+++ counter.js
@@ -4,3 +4,19 @@ var timer = document.getElementById("timer");
 var greeting = "Hello world!";
 var d = new Date();
 timer.innerHTML = greeting;
+1/8
+
+
+type AppProps = {
+    message: string;
+    count: number;
+    disabled: boolean;
+    /** array of a type! */
+    names: string[];
+    /** string literals to specify exact string values, with a union type to join them together */
+    status: "waiting" | "success";
+    /** an object with known properties (but could have more at runtime) */
+    obj: {
+      id: string;
+      title: string;
+    };
